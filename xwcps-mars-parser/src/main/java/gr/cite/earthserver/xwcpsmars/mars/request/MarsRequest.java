0af4c09b9af4c899bf59a98cf41fe62d3db24c96
Konstantinos Apostolopoulos <kapostolopoulos@cite.gr>
2017-04-26 19:00:37 +0300

    MarsRequest helper class

---
+++ xwcps-mars-parser/src/main/java/gr/cite/earthserver/xwcpsmars/mars/request/MarsRequest.java
@@ -0,0 +1,141 @@
+package gr.cite.earthserver.xwcpsmars.mars.request;
+
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
+public class MarsRequest {
+	@JsonProperty("class")
+	private String classification;
+
+	@JsonProperty("area")
+	private String area;
+
+	@JsonProperty("dataset")
+	private String dataset;
+
+	@JsonProperty("date")
+	private String date;
+
+	@JsonProperty("expver")
+	private String expver;
+
+	@JsonProperty("grid")
+	private String grid;
+
+	@JsonProperty("levtype")
+	private String levtype;
+
+	@JsonProperty("param")
+	private String param;
+
+	@JsonProperty("step")
+	private String step;
+
+	@JsonProperty("stream")
+	private String stream;
+
+	@JsonProperty("time")
+	private String time;
+
+	@JsonProperty("type")
+	private String type;
+
+
+	public String getClassification() {
+		return classification;
+	}
+
+	public void setClassification(String classification) {
+		this.classification = classification;
+	}
+
+	public String getArea() {
+		return area;
+	}
+
+	public void setArea(String area) {
+		this.area = area;
+	}
+
+	public String getDataset() {
+		return dataset;
+	}
+
+	public void setDataset(String dataset) {
+		this.dataset = dataset;
+	}
+
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+	public String getExpver() {
+		return expver;
+	}
+
+	public void setExpver(String expver) {
+		this.expver = expver;
+	}
+
+	public String getGrid() {
+		return grid;
+	}
+
+	public void setGrid(String grid) {
+		this.grid = grid;
+	}
+
+	public String getLevtype() {
+		return levtype;
+	}
+
+	public void setLevtype(String levtype) {
+		this.levtype = levtype;
+	}
+
+	public String getParam() {
+		return param;
+	}
+
+	public void setParam(String param) {
+		this.param = param;
+	}
+
+	public String getStep() {
+		return step;
+	}
+
+	public void setStep(String step) {
+		this.step = step;
+	}
+
+	public String getStream() {
+		return stream;
+	}
+
+	public void setStream(String stream) {
+		this.stream = stream;
+	}
+
+	public String getTime() {
+		return time;
+	}
+
+	public void setTime(String time) {
+		this.time = time;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+}
